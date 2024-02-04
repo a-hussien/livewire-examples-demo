cc933a6159573cb41b23933cd24d6687f25b6da8
Ahmed Hussein <a.hussien87@gmail.com>
2024-02-04 16:50:34 +0200

    Created Note Model and migration

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(50),
+            'description' => fake()->text(),
+            'send_date' => fake()->dateTimeBetween('-1 years'),
+            'published' => fake()->boolean(),
+            'impressions' => fake()->numberBetween(0, 1000),
+            'user_id' => User::factory(),
+        ];
+    }
+}
---
+++ database/migrations/2024_02_04_135653_create_notes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('title');
+            $table->longText('description');
+            $table->date('send_date');
+            $table->boolean('published')->default(false);
+            $table->integer('impressions')->default(0);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
